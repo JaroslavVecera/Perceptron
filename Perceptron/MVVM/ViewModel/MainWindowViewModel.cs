647c0dd0c5f2c5eba8b4395b8721930df1330f50
Jaroslav Večeřa <jarekvecer@seznam.cz>
2022-10-25 14:54:30 +0200

    switching views resolves #2

--- Perceptron/MVVM/ViewModel/MainWindowViewModel.cs
+++ Perceptron/MVVM/ViewModel/MainWindowViewModel.cs
@@ -9,6 +9,47 @@ namespace Perceptron.MVVM.ViewModel
 {
     class MainWindowViewModel : ObservableObject
     {
+        ObservableObject _currentView;
 
+        DirectInputViewModel DirectInputView { get; set; }
+        ImageInputViewModel ImageInputView { get; set; }
+
+        public ObservableObject CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public RelayCommand DirectInputViewCommand { get; set; }
+        public RelayCommand ImageInputViewCommand { get; set; }
+
+
+        public MainWindowViewModel()
+        {
+            InitializeViews();
+            InitializeCommands();
+            CurrentView = DirectInputView;
+        }
+
+        void InitializeViews()
+        {
+            DirectInputView = new DirectInputViewModel();
+            ImageInputView = new ImageInputViewModel();
+        }
+
+        void InitializeCommands()
+        {
+            DirectInputViewCommand = new RelayCommand((x) =>
+            {
+                CurrentView = DirectInputView;
+            });
+            ImageInputViewCommand = new RelayCommand((x) =>
+            {
+                CurrentView = ImageInputView;
+            });
+        }
     }
 }
