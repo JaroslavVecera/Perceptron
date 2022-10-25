5b4ab707dcbf9223e3f5fea5fdc507d999fa8c57
Jaroslav Večeřa <jarekvecer@seznam.cz>
2022-10-25 20:13:19 +0200

    make custome title bar resolves #6

--- Perceptron/MainWindow.xaml.cs
+++ Perceptron/MainWindow.xaml.cs
@@ -31,5 +31,29 @@ namespace Perceptron
         {
             InitializeComponent();
         }
+
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            else
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
